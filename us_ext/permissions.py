dc11cf1c791a203a3447d162e82c56893dea87b2
Denis Akhmedov <denis.akhmedov86@gmail.com>
2023-06-19 15:57:40 +0300

    edit permissions.py file

--- us_ext/permissions.py
+++ us_ext/permissions.py
@@ -9,4 +9,10 @@ class IsAllowedAddress(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        return request.META.get('REMOTE_ADDR') in ALLOWED_ADDRESSES
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        return ip in ALLOWED_ADDRESSES
